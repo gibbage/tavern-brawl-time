806c5c7980175f8dbbd137607a3a87fc748914ac
Shane Gibb <shane.gibb@gmail.com>
2016-03-11 22:59:31 +1100

    Extract UI into a module
    Just a little house keeping.

--- src/app.js
+++ src/app.js
@@ -1,20 +1,15 @@
-var UI = require('ui');
 var ajax = require('ajax');
+var BrawlUI = require('./brawl-ui');
 var ConfigManager = require('./config-manager');
 var TimelineManager = require('./timeline-manager');
 var BrawlScheduler = require('./brawl-scheduler');
 
 var URL = 'https://s3-us-west-2.amazonaws.com/tavern-brawl-time/current.json';
 
-// Loading screen
-var card = new UI.Card({
-  title:'It\'s Tavern Brawl Time',
-  body:'Hold on to your butts...',
-  scrollable: true
-});
-card.show();
-
 console.log('App ready!');
+
+var brawlUI = new BrawlUI();
+brawlUI.showLoadingScreen();
 
 var region = ConfigManager.getRegion();
 TimelineManager.subscribeToTopic(region);
@@ -24,30 +19,23 @@ ConfigManager.addOnRegionChangeListener(loadMainScreen);
 loadMainScreen(region);
 
 function loadMainScreen(region) {
-  var footer = ' (Q‘-‘)=O  O=(‘-‘Q)';
   var scheduler = new BrawlScheduler(region);
-  if (scheduler.brawlStillActive()) {
+  if (!scheduler.brawlStillActive()) {
     ajax(
       {
         url: URL,
         type: 'json'
       },
-      function (data) {
-        console.log('Successfully fetched Brawl data.', data);
-        card.title(data.name);
-        delete data.type;
-        var finishText = '* Ends ' + scheduler.timeUntilNextEvent() + '\n';
-        var deckTypeText = data.type ? '* ' + data.type + '\n' : '';
-        var quoteText = data.quote ? '"' + data.quote + '"\n' : '';
-        card.body(finishText + deckTypeText + quoteText + footer);
+      function onSuccess(data) {
+        data.timeUntilFinished = scheduler.timeUntilNextEvent();
+        brawlUI.showBrawlDetails(data);
       },
-      function(error) {
+      function onError(error) {
         console.log('Failed fetching Brawl data: ' + error);
-        card.body('There was an error! Please try again later.\n' + error);
+        brawlUI.showError(error);
       }
     );
   } else {
-    card.title('Brawl finished');
-    card.body('\nBack ' + scheduler.timeUntilNextEvent() + '!\n\n' + footer);
+    brawlUI.showBrawlClosed(scheduler.timeUntilNextEvent());
   }
 }
---
+++ src/brawl-ui.js
@@ -0,0 +1,36 @@
+var UI = require('ui');
+
+var FOOTER = ' (Q‘-‘)=O  O=(‘-‘Q)';
+
+function BrawlUI () {
+  this.card = new UI.Card({
+    scrollable: true
+  });
+}
+
+BrawlUI.prototype = {
+  showLoadingScreen: function () {
+    this.card.title('It\'s Tavern Brawl Time');
+    this.card.body('Hold on to your butts...');
+    this.card.show();
+  },
+  showBrawlDetails: function (data) {
+    var finishText = '* Ends ' + data.timeUntilFinished + '\n';
+    var deckTypeText = data.type ? '* ' + data.type + '\n' : '';
+    var quoteText = data.quote ? '"' + data.quote + '"\n' : '';
+    this.card.title(data.name);
+    this.card.body(finishText + deckTypeText + quoteText + FOOTER);
+    this.card.show();
+  },
+  showError: function (error) {
+    this.card.body('There was an error! Please try again later.\n' + error);
+    this.card.show();
+  },
+  showBrawlClosed: function (timeUntilNextBrawl) {
+    this.card.title('Brawl finished');
+    this.card.body('\nBack ' + timeUntilNextBrawl + '!\n\n' + FOOTER);
+    this.card.show();
+  }
+};
+
+module.exports = BrawlUI;
