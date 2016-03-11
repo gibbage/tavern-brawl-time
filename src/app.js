1070bc59bb7bd00e14066bf4cd5874a5a0784f7b
Shane Gibb <shane.gibb@gmail.com>
2016-03-11 21:53:28 +1100

    Add Brawl Scheduler and many other things
    This commit got out of hand. Its hard to manage them properly when using CloudPebble.

--- src/app.js
+++ src/app.js
@@ -2,36 +2,48 @@ var UI = require('ui');
 var ajax = require('ajax');
 var ConfigManager = require('./config-manager');
 var TimelineManager = require('./timeline-manager');
+var BrawlScheduler = require('./brawl-scheduler');
 
 var URL = 'https://s3-us-west-2.amazonaws.com/tavern-brawl-time/current.json';
-
-Pebble.addEventListener('ready', function(e) {
-  console.log('App ready!');
-  var region = ConfigManager.getRegion();
-  TimelineManager.subscribeToTopic(region);
-  ConfigManager.addOnRegionChangeListener(TimelineManager.subscribeToTopic);
-});
 
 // Loading screen
 var card = new UI.Card({
   title:'It\'s Tavern Brawl Time',
-  subtitle:'Hold on to your buttz...',
+  body:'Hold on to your butts...',
   scrollable: true
 });
 card.show();
 
-ajax(
-  {
-    url: URL,
-    type: 'json'
-  },
-  function (data) {
-    console.log('Successfully fetched Brawl data!');
-    card.title(data.name);
-    card.subtitle(data.quote);
-    card.body(data.overview);
-  },
-  function(error) {
-    console.log('Failed fetching Brawl data: ' + error);
+console.log('App ready!');
+
+var region = ConfigManager.getRegion();
+TimelineManager.subscribeToTopic(region);
+ConfigManager.addOnRegionChangeListener(TimelineManager.subscribeToTopic);
+ConfigManager.addOnRegionChangeListener(loadMainScreen);
+
+loadMainScreen(region);
+
+function loadMainScreen(region) {
+  var scheduler = new BrawlScheduler(region);
+  if (scheduler.brawlStillActive()) {
+    ajax(
+      {
+        url: URL,
+        type: 'json'
+      },
+      function (data) {
+        console.log('Successfully fetched Brawl data.', data);
+        card.title(data.name);
+        var finishText = 'Ends ' + scheduler.timeUntilNextEvent();
+        card.body(finishText + '\n"' + data.quote + '"');
+      },
+      function(error) {
+        console.log('Failed fetching Brawl data: ' + error);
+        card.body('There was an error! ' + error);
+      }
+    );
+  } else {
+    card.title('Brawl finished');
+    card.body('Back ' + scheduler.timeUntilNextEvent());
   }
-);
+}
---
+++ src/brawl-scheduler.js
@@ -0,0 +1,45 @@
+var moment = require('vendor/moment');
+
+var REGIONS = {
+  'americas': {
+    start: { day: 'wednesday', hour: 17 },
+    finish: { day: 'monday', hour: 11 }
+  },
+  'europe': {
+    start: { day: 'wednesday', hour: 21 },
+    finish: { day: 'monday', hour: 5 }
+  },
+  'korea': { 
+    start: { day: 'wednesday', hour: 21 },
+    finish: { day: 'sunday', hour: 22 }
+  },
+  'taiwan-china': {
+    start: { day: 'wednesday', hour: 21 },
+    finish: { day: 'sunday', hour: 22 }
+  }
+};
+
+function BrawlScheduler (region) {
+  this.startTimestamp = getNextEventTimestamp(REGIONS[region].start);
+  this.finishTimestamp = getNextEventTimestamp(REGIONS[region].finish);
+}
+
+BrawlScheduler.prototype = {
+  brawlStillActive: function () {
+    return this.finishTimestamp.isBefore(this.startTimestamp);
+  },
+  timeUntilNextEvent: function () {
+    return this.brawlStillActive() ? this.finishTimestamp.fromNow() : this.startTimestamp.fromNow();
+  }
+};
+
+function getNextEventTimestamp(timeConfig) {
+  var now = moment();
+  var nextEventTimestamp = moment().utc().set(timeConfig).startOf('hour');
+  if (nextEventTimestamp.isBefore(now)) {
+    nextEventTimestamp.add(1, 'week');
+  }
+  return nextEventTimestamp;
+}
+
+module.exports = BrawlScheduler;
